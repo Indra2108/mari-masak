943c5ea5a2b03e7ec9bdbbbd42c3e4550f21a30f
Indra2108 <fizipratama21@gmail.com>
2021-11-27 09:57:58 +0700

    Add search.png

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,13 +1,19 @@
 import React from "react";
-import { View } from 'react-native';
+import { Image, Text, View } from 'react-native';
 
 // Import styles for styling
 import styles from './styles';
 
+// Import image
+import lup from './assets/search.png';
+
 export default Home = () => {
     return(
         <View style={styles.container}>
-            
+            <View style={styles.backgroundSearch}>
+                <Image source={lup} style={styles.lups}/>
+                <Text style={styles.textSearch}>Search</Text>
+            </View>
         </View>
     )
 }
--- src/Home/styles.js
+++ src/Home/styles.js
@@ -2,6 +2,18 @@ import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
     container: {
-        flex: 1
+        flex: 1,
+        padding: '10%'
+    },
+    backgroundSearch: {
+        padding: '10%',
+        backgroundColor: '#f4f4f4',
+        flexDirection: 'row',
+    },
+    lups: {
+        width: '100%'
+    },
+    textSearch: {
+        
     }
 })
