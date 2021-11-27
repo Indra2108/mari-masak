98eab2cac09ee11fee73b1f41b0e28dce17f3db3
Indra2108 <fizipratama21@gmail.com>
2021-11-27 12:29:55 +0700

    Mengpusing Add Category button

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Image, Text, TouchableOpacity, View } from 'react-native';
+import { Image, ScrollView, Text, TouchableOpacity, View } from 'react-native';
 
 // Import styles for styling
 import styles from './styles';
@@ -9,15 +9,58 @@ import lup from './assets/search.png';
 
 export default Home = () => {
 
-    
+    const Category = (props) => {
+        return (
+            <TouchableOpacity style={styles.backgroundCategory}>
+
+                <Text style={styles.nameCategory}>{props.nama}</Text>
+            </TouchableOpacity>
+        )
+    }
+
+    const Genre = (props) => {
+        return (
+            <TouchableOpacity style={{ backgroundColor: '#28293d', padding: 15, marginTop: '2%', marginRight: '2%', borderTopLeftRadius: 25, borderTopRightRadius: 25, borderBottomRightRadius: 25 }}>
+                <Text style={{ fontWeight: 'bold', color: '#e176d3' }}>{props.genre}</Text>
+            </TouchableOpacity>
+        )
+    }
 
     return (
         <View style={styles.container}>
-            <TouchableOpacity style={styles.backgroundSearch}>
-                <Image source={lup} style={styles.lups} />
-                <Text style={styles.textSearch}>Search</Text>
-            </TouchableOpacity>
+            <View style={styles.container2}>
+                <TouchableOpacity style={styles.backgroundSearch}>
+                    <Image source={lup} style={styles.lups} />
+                    <Text style={styles.textSearch}>Search</Text>
+                </TouchableOpacity>
+            </View>
 
-        </View>
+            <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
+                <ScrollView horizontal contentContainerStyle={{ marginLeft: '3%' }}>
+                    <Genre genre='Action' style={{ marginLeft: '10%' }} />
+                    <Genre genre='Adventure' />
+                    <Genre genre='Comedy' />
+                    <Genre genre='Demons' />
+                    <Genre genre='Drama' />
+                    <Genre genre='Fantasy' />
+                </ScrollView>
+            </View>
+
+            {/* <View style={styles.viewCategories}>
+                <ScrollView horizontal contentContainerStyle={styles.scroolViewCategories}>
+                <ScrollView horizontal>
+                <Category nama='Dessert' />
+                <Category nama='Masakan Hari Raya' />
+                <Category nama='Masakan Tradisional' />
+                <Category nama='Menu Makan Malam' />
+                <Category nama='Menu Makan Siang' />
+                <Category nama='Resep Ayam' />
+                <Category nama='Resep Daging' />
+                <Category nama='Resep Sayuran' />
+                <Category nama='Resep Seafood' />
+                <Category nama='Sarapan' />
+                </ScrollView>
+            </View> */}
+        </View >
     )
 }
--- src/Home/styles.js
+++ src/Home/styles.js
@@ -3,7 +3,10 @@ import { StyleSheet } from "react-native";
 export default StyleSheet.create({
     container: {
         flex: 1,
-        padding: '8%'
+    },
+    container2: {
+        // flex: 1,
+        padding: '7%',
     },
     backgroundSearch: {
         padding: '4%',
@@ -15,11 +18,32 @@ export default StyleSheet.create({
     },
     lups: {
         width: '10%',
-        height: '100%', 
+        height: '100%',
         marginRight: '6%'
     },
     textSearch: {
         fontFamily: 'Poppins-Light',
         fontSize: 16
+    },
+    backgroundCategory: {
+        paddingHorizontal: '8%',
+        paddingVertical: '2%',
+        backgroundColor: '#94c656',
+        borderRadius: 20,
+        marginRight: '3%',
+    },
+    nameCategory: {
+        fontFamily: 'Poppins-Light',
+        color: 'white'
+    },
+    viewCategories: {
+        flexDirection: 'row',
+        // justifyContent: 'space-between',
+        flexWrap: 'wrap'
+    },
+    scroolViewCategories: {
+        marginLeft: '4.5%',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
     }
 })
