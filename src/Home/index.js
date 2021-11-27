36b0359c27b533c0267d3165dabf7d456400e534
Indra2108 <fizipratama21@gmail.com>
2021-11-27 09:31:09 +0700

    Updating code...

---
+++ src/Home/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View } from 'react-native';
+
+// Import styles for styling
+import styles from './styles';
+
+export default Home = () => {
+    return(
+        <View style={styles.container}>
+            
+        </View>
+    )
+}
