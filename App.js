9a89d60df6e318d558e2fffffc20ac534f242917
Indra2108 <fizipratama21@gmail.com>
2022-03-13 08:09:30 +0700

    Navigation: Update Navigation route Add Splashscreen src/Search: Change header title to 'Cari resep'

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 // Import screen
+import Splash from './src/Splash';
 import Home from './src/Home';
 import Content from './src/Content';
 import Search from './src/Search';
@@ -15,10 +16,11 @@ const Stack = createNativeStackNavigator();
 export default App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Home'>
+      <Stack.Navigator initialRouteName='Splash'>
+        <Stack.Screen name="Splash" component={Splash} options={{ headerShown: false }}/>
         <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
         <Stack.Screen name="Content" component={Content} options={{ headerShown: false }} />
-        <Stack.Screen name="Search" component={Search} options={{ title: 'Cari resep' }}/>
+        <Stack.Screen name="Search" component={Search} options={{ title: 'Cari resep' }} />
         <Stack.Screen name="Wishlist" component={Wishlist} />
         <Stack.Screen name="Article" component={Article} />
         <Stack.Screen name="ContentArticle" component={ContentArticle} />
